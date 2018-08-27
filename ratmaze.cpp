17d3faed93b7e59330accd84af573d3a3ecb941c
Ankit <ankitdimri1999@gmail.com>
2018-08-27 23:47:43 +0530

    added comments to ratmaze

--- ratmaze.cpp
+++ ratmaze.cpp
@@ -1,3 +1,22 @@
+/*******************************************************
+ Name : Ankit dimri
+ Algorithm : Backtracking
+
+ A Maze is given as N*N binary matrix of blocks where source block is the upper
+ left most block i.e., maze[0][0] and destination block is lower rightmost block
+ i.e., maze[N-1][N-1]. A rat starts from source and has to reach the destination.
+ The rat can move only in two directions: forward and down.
+ In the maze matrix, 0 means the block is a dead end and 1 means the block can
+ be used in the path from source to destination.
+
+ maze :  1 0 0 0
+         1 1 0 1
+         0 1 0 0
+         1 1 1 1
+
+********************************************************/
+
+
 #include <iostream>
 #include <vector>
 #include <utility>
